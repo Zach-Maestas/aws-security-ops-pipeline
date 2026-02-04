08fd9de2540ec9880d49e55de4009ced4a4d53cb
Zach-Maestas <zachary.maestas03@gmail.com>
2026-02-03 18:13:36 -0700

    feat(backend): added S3 bucket and DynamoDB table for remote backend

---
+++ infrastructure/terraform/backend-state-init/main.tf
@@ -0,0 +1,41 @@
+resource "aws_s3_bucket" "tfstate" {
+    bucket = "devsecops-security-ops-tfstate-bucket"
+}
+
+resource "aws_dynamodb_table" "tflock" {
+    name         = "terraform-lock"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key     = "LockID"
+
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
+
+resource "aws_s3_bucket_versioning" "tfstate-versioning" {
+    bucket = aws_s3_bucket.tfstate.id
+
+    versioning_configuration {
+        status = "Enabled"
+    }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf-state-sse" {
+    bucket = aws_s3_bucket.tfstate.id
+
+    rule {
+        apply_server_side_encryption_by_default {
+            sse_algorithm = "AES256"
+        }
+    }
+}
+
+resource "aws_s3_bucket_public_access_block" "tf-state-block-access" {
+    bucket = aws_s3_bucket.tfstate.id
+
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
+}
