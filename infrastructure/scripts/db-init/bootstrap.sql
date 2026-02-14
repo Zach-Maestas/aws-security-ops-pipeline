8a0c202d22fccf2a55e01a3aaa3d7acd1d7f8c2b
Zach-Maestas <zachary.maestas03@gmail.com>
2026-02-14 02:28:17 -0700

    chore(scripts): script cleanup, fixed region convention in 01, changed comments, added logging level env var passed from terraform ecs task def

--- infrastructure/scripts/db-init/bootstrap.sql
+++ infrastructure/scripts/db-init/bootstrap.sql
@@ -4,15 +4,13 @@ BEGIN;
 -- Lock down default PUBLIC privileges
 REVOKE ALL ON SCHEMA public FROM PUBLIC;
 
--- Create the app role if it doesn't exist (uses psql meta-commands since
--- psql variable substitution doesn't work inside DO $$ PL/pgSQL blocks)
+-- Create the app role if it doesn't exist
 SELECT count(*) = 0 AS needs_create FROM pg_roles WHERE rolname = :'app_username' \gset
 \if :needs_create
 CREATE ROLE :"app_username" LOGIN;
 \endif
 
 -- Set password using psql variable (passed via psql -v app_password="...")
--- This always updates the password, which is good for secret rotation
 ALTER ROLE :"app_username" WITH PASSWORD :'app_password';
 
 -- Grant least privilege to the app role
